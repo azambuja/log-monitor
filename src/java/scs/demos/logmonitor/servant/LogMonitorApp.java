469b59f489b98d42036589cdf59f32da80cefd85
Rafael Pereira <rafaelpereira@rafael-pereiras-macbook.local>
2008-10-21 23:00:08 -0200

    implementcao do canal de eventos, e conexao do mesmo com o log monitor e log viewer

--- src/java/scs/demos/logmonitor/servant/LogMonitorApp.java
+++ src/java/scs/demos/logmonitor/servant/LogMonitorApp.java
@@ -3,6 +3,9 @@ package scs.demos.logmonitor.servant;
 import org.omg.CORBA.ORB;
 import org.omg.CORBA.Object;
 import org.omg.CORBA.SystemException;
+import org.omg.CORBA.Any;
+import org.omg.PortableServer.POA;
+import org.omg.PortableServer.POAHelper;
 
 import scs.container.ComponentAlreadyLoaded;
 import scs.container.ComponentCollection;
@@ -13,17 +16,33 @@ import scs.container.ComponentLoader;
 import scs.container.ComponentLoaderHelper;
 import scs.container.ComponentNotFound;
 import scs.container.LoadFailure;
+import scs.event_service.ChannelCollection;
+import scs.event_service.ChannelCollectionHelper;
+import scs.event_service.ChannelDescr;
+import scs.event_service.ChannelFactory;
+import scs.event_service.ChannelFactoryHelper;
+import scs.event_service.ChannelManagement;
+import scs.event_service.ChannelManagementHelper;
+import scs.event_service.EventSink;
+import scs.event_service.EventSinkHelper;
+import scs.event_service.NameAlreadyInUse;
+import scs.event_service.servant.ConnectionStatus;
+import scs.event_service.servant.EventSinkConsumerServant;
 import scs.core.AlreadyConnected;
 import scs.core.ExceededConnectionLimit;
 import scs.core.IComponent;
 import scs.core.IComponentHelper;
 import scs.core.IReceptacles;
 import scs.core.IReceptaclesHelper;
+import scs.core.IMetaInterface;
+import scs.core.IMetaInterfaceHelper;
 import scs.core.InvalidConnection;
 import scs.core.InvalidName;
 import scs.core.StartupFailed;
+import scs.core.FacetDescription;
 import scs.demos.logmonitor.LogMonitor;
 import scs.demos.logmonitor.LogMonitorHelper;
+import scs.demos.logmonitor.servant.EventSinkViewerServant;
 import scs.execution_node.ContainerAlreadyExists;
 import scs.execution_node.ExecutionNode;
 import scs.execution_node.ExecutionNodeHelper;
@@ -37,8 +56,9 @@ public class LogMonitorApp {
 	private static final String CONTAINER_NAME = "LogMonitorDemoContainer";
 
 	private ExecutionNode[] execNode = null;
-	private IComponent logMonitorComponent1 = null;
-	private IComponent logMonitorComponent2 = null;
+	private IComponent logMonitorComp = null;
+	private IComponent logViewerComp = null;
+	private String exception;
 
 	public LogMonitorApp(String[] args) {
 		if (!initialize(args))
@@ -70,6 +90,27 @@ public class LogMonitorApp {
 		}
 		return true;
 	}
+	
+	
+	/* Cria um component no container associado a loader*/
+	private ComponentHandle createHandle(ComponentLoader loader, ComponentId compId){
+		ComponentHandle handle = null;
+
+		try {
+			handle = loader.load(compId, new String[] { "" });
+			handle.cmp.startup();
+		} catch (ComponentNotFound e) {
+			System.out.println("WorkerInitializer::createHandle - Componente " + compId.name + " nao encontrado.");
+		} catch (ComponentAlreadyLoaded e) {
+			System.out.println("WorkerInitializer::createHandle - Componente " + compId.name + " já foi criado.");
+		} catch (LoadFailure e) {
+			System.out.println("WorkerInitializer::createHandle - Erro ao carregar componente " + compId.name + ".\n");
+		} catch (StartupFailed e) {
+			System.out.println("WorkerInitializer::createHandle - Startup do componente " + compId.name + " falhou.\n");
+		}
+
+		return handle;
+	}
 
 	/**
 		* @param args
@@ -80,13 +121,6 @@ public class LogMonitorApp {
 		String corbaname = null;
 		int id = 1;
 		int numComponentPerNode;
-
-/*		if (args.length > 0 && args.length < 4) { 
-			System.out.println("Para criar os componentes em maquinas diferentes:");
-			System.out.println("   Syntax: PingPongApp <host-1> <port-1> <host-2> <port-2>");
-			System.exit(1);
-		}
-*/
 
 		execNode = new ExecutionNode[1];
 		args = new String[2];
@@ -142,76 +176,131 @@ public class LogMonitorApp {
 				return false;
 			}
 
-			Object componentCollection = container
-				.getFacet("scs::container::ComponentCollection");
-			ComponentCollection components = ComponentCollectionHelper
-				.narrow(componentCollection);
-
+			ComponentHandle logMonitorHandle = null;
 			ComponentId logMonitorCompId = new ComponentId();
 			logMonitorCompId.name = "LogMonitor";
 			logMonitorCompId.version = 1;
 
-			ComponentHandle logMonitorHandle = null;
-
-			for (int j=0; j < numComponentPerNode; j++) {
-				try {
-					logMonitorHandle = loader.load(logMonitorCompId, new String[] { "" });
-				} catch (ComponentNotFound e) {
-					System.err.println("COMPONENTE NAO ENCONTRADO !");
-					e.printStackTrace();
-					return false;
-				} catch (ComponentAlreadyLoaded e) {
-					ComponentHandle handles[] = components.getComponent(logMonitorCompId);
-					logMonitorHandle = handles[0];
-				} catch (LoadFailure e) {
-					e.printStackTrace();
-					return false;
-				}
-
-				try {
-					logMonitorHandle.cmp.startup();
-				} catch (StartupFailed e) {
-					e.printStackTrace();
-					return false;
-				}
-
-				LogMonitor log = LogMonitorHelper.narrow(logMonitorHandle.cmp
-					.getFacetByName("LogMonitor"));
-
-				log.setId(id);
-				System.out.println("LogMonitor id =" + id + " carregado com sucesso.");
-
-				if (id ==1)
-					logMonitorComponent1 = logMonitorHandle.cmp;
-				else
-					logMonitorComponent2 = logMonitorHandle.cmp;
-
-				id++;	
+			logMonitorHandle = createHandle(loader, logMonitorCompId);
+			if (logMonitorHandle == null) {
+				return false;
 			}
+
+			logMonitorComp = logMonitorHandle.cmp;
+
+			IReceptacles info1 = IReceptaclesHelper.narrow(logMonitorComp.getFacetByName("infoReceptacle"));
+			if( info1 == null ) {
+				System.out.println("WorkerInitializer::buildChannel - Erro ao retornar receptaculo do LogMonitor !");
+				return false;
+			}
+
+			LogMonitor logMonitor = LogMonitorHelper.narrow(logMonitorComp.getFacet("scs::demos::logmonitor::LogMonitor"));
+			if( logMonitor == null ) {
+				System.out.println("WorkerInitializer::buildChannel - Erro ao retornar LogMonitor !");
+				return false;
+			}
+			logMonitor.setId(1);
+			System.out.println("Id do logMonitor: " + logMonitor.getId());
+
+
+			ComponentHandle eventMgrHandle = null;
+			ComponentId eventMgrCompId = new ComponentId();
+			eventMgrCompId.name = "EventManager";
+			eventMgrCompId.version = 1;
+
+			eventMgrHandle = createHandle(loader, eventMgrCompId);
+			if (eventMgrHandle == null) {
+				return false;
+			}
+
+			IComponent eventMgr = eventMgrHandle.cmp;
+		
+			ComponentHandle logViewerHandle = null;
+			ComponentId logViewerCompId = new ComponentId();
+			logViewerCompId.name = "LogViewer";
+			logViewerCompId.version = 1;
+
+			logViewerHandle = createHandle(loader, logViewerCompId);
+			if (logViewerHandle == null) {
+				return false;
+			}
+
+			logViewerComp = logViewerHandle.cmp;
+
+			EventSink eventSinkViewer = EventSinkHelper.narrow(logViewerComp.getFacet("scs::demos::logmonitor::EventSink"));
+			if( logViewerComp.getFacetByName("EventSink") == null ) {
+				System.out.println("WorkerInitializer::buildChannel - Erro ao retornar eventSinkViewer !");
+				return false;
+			}
+			
+			ChannelFactory chFactory = ChannelFactoryHelper.narrow(eventMgr.getFacet("scs::event_service::ChannelFactory"));
+			if( chFactory== null ) {
+				System.out.println("WorkerInitializer::buildChannel - Erro ao retornar ChannelFactory !");
+				return false;
+			}
+			
+			ChannelCollection chCollection = ChannelCollectionHelper.narrow(eventMgr.getFacet("scs::event_service::ChannelCollection"));
+			if( chCollection == null ) {
+				System.out.println("WorkerInitializer::buildChannel - Erro ao retornar ChannelCollection !");
+				return false;
+			}
+
+			IComponent masterChannel = null;
+
+			try {
+				masterChannel = chFactory.create("MasterChannel");
+				masterChannel.startup();
+				EventSink eventSink = EventSinkHelper.narrow(masterChannel.getFacetByName("EventSink"));
+				IReceptacles eventSource = IReceptaclesHelper.narrow(masterChannel.getFacet("scs::core::IReceptacles"));
+				/*IMetaInterface aaa = IMetaInterfaceHelper.narrow(logViewerComp.getFacet("scs::core::IMetaInterface"));
+				 
+				FacetDescription[] facets = aaa.getFacets();
+
+				for (int k = 0; k < facets.length; k++) {
+					FacetDescription description = facets[k];
+					System.out.println("Faceta: " + description.name);
+				}*/
+				
+				
+				try {
+					info1.connect("LogMonitor", eventSink);
+				} catch (InvalidName e) {
+					e.printStackTrace();
+				} catch (InvalidConnection e) {
+					e.printStackTrace();
+				} catch (AlreadyConnected e) {
+					e.printStackTrace();
+				} catch (ExceededConnectionLimit e) {
+					e.printStackTrace();
+				}
+				
+				try {
+					eventSource.connect("EventSource", eventSinkViewer);
+				} catch (Exception e) {
+					System.out.println("WorkerInitializer::buildChannel - Erro ao conectar source no sink." + e.getMessage());
+					return false;
+				}
+				
+				ChannelDescr channels[] = chCollection.getAll();
+
+				for (int j = 0; j < channels.length; j++) {
+					ChannelDescr ch = channels[j];
+					System.out.println("Canal: " + ch.name + " criado");
+
+				}
+				
+				
+			} catch (Exception e) {
+				System.out.println("WorkerInitializer::buildChannel - Erro ao instanciar channel.\n");
+				return false;
+			}
+		
 		}
-
-		IReceptacles info1 = IReceptaclesHelper.narrow(logMonitorComponent1
-			.getFacetByName("infoReceptacle"));
-		IReceptacles info2 = IReceptaclesHelper.narrow(logMonitorComponent2
-			.getFacetByName("infoReceptacle"));
-
-		LogMonitor log1 = LogMonitorHelper.narrow(logMonitorComponent1
-			.getFacetByName("LogMonitor"));
-		LogMonitor log2 = LogMonitorHelper.narrow(logMonitorComponent2
-			.getFacetByName("LogMonitor"));
-
-		try {
-			info1.connect("LogMonitor", log1);
-			info2.connect("LogMonitor", log2);
-		} catch (InvalidName e) {
-			e.printStackTrace();
-		} catch (InvalidConnection e) {
-			e.printStackTrace();
-		} catch (AlreadyConnected e) {
-			e.printStackTrace();
-		} catch (ExceededConnectionLimit e) {
-			e.printStackTrace();
-		}
+		
+		
+		
+		
+		
 		return true;
 	}
 
@@ -220,9 +309,9 @@ public class LogMonitorApp {
 			long start = System.currentTimeMillis();
 			LogMonitorApp app = new LogMonitorApp(args);
 			app.run();
-			app.stop();
 			long end = System.currentTimeMillis();
 			System.out.println("Tempo total de execucao:" + (end - start));
+			app.stop();
 		} catch (SystemException ex) {
 			ex.printStackTrace();
 		} catch (Exception ex) {
@@ -231,23 +320,15 @@ public class LogMonitorApp {
 
 	}
 
-	/**
-		* * @throws InterruptedException
-		* * 
-		* */
 	public void run() throws InterruptedException {
-		LogMonitor log1 = LogMonitorHelper.narrow(logMonitorComponent1
-			.getFacetByName("LogMonitor"));
-		LogMonitor log2 = LogMonitorHelper.narrow(logMonitorComponent2
-			.getFacetByName("LogMonitor"));
-
-		log2.start();
-		log1.start();
+		LogMonitor log = LogMonitorHelper.narrow(logMonitorComp.getFacetByName("LogMonitor"));
+		Any eventAny = ORB.init().create_any();
+		eventAny.insert_string("Teste");
+		log.publishLog(eventAny);
 
 	}
 
 	public void stop() {
-
 		try{
 			for(int i=0; i<execNode.length; i++) {
 				execNode[i].stopContainer(CONTAINER_NAME); 
